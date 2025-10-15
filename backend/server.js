17c5cf104525f9c5b32fafaaae898b0e9998fb63
Kinoshan Ketheeswaran <kinoshanketheeswaran01@gmail.com>
2025-10-15 18:00:38 +0530

    cors changes

--- backend/server.js
+++ backend/server.js
@@ -20,24 +20,13 @@ const allowedOrigins = [
 
 // CORS middleware
 app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true); // Allow non-browser requests (Postman, curl)
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: allowedOrigins,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: [
-    'Content-Type', 
-    'Authorization', 
-    'X-Requested-With',
-    'Accept',
-    'Origin'
-  ]
+  methods: ['GET','POST','PUT','DELETE','OPTIONS','PATCH'],
+  allowedHeaders: ['Content-Type','Authorization','X-Requested-With','Accept','Origin']
 }));
+
+
 dotenv.config();
 
 const connectDB = require("./config/db");
